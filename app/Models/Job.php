7a88b396afab02fb1a7718a7e9f556ab8dd19437
Liakat Biswas <liakatbiswas@gmail.com>
2024-09-03 15:19:00 +0600

    Meet Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+// class JobListing extends Model
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40000',
-            ],
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-
-    // return response()->json([
-    //     'jobs' => [
-    //         [
-    //             'title' => 'Director',
-    //             'salary' => '$50000',
-    //         ],
-    //         [
-    //             'title' => 'Programmer',
-    //             'salary' => '$10000',
-    //         ],
-    //         [
-    //             'title' => 'Teacher',
-    //             'salary' => '$40000',
-    //         ],
-    //     ],
-    // ]);
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    return view(
+        'jobs',
+        [
+            'jobs' => Job::all(),
+        ]
+    );
 });
 
 Route::get('/contact', function () {
@@ -35,5 +23,8 @@ Route::get('/contact', function () {
 Route::get('jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view(
+        'job',
+        ['job' => $job]
+    );
 });
