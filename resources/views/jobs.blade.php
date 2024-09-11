be80cc320558bf1d794cc78d331e01ec432990f3
Liakat Biswas <liakatbiswas@gmail.com>
2024-09-11 15:51:51 +0600

    Eager Loading and the N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,18 @@
         Jobs Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
                     <strong> {{ $job['title'] }}</strong> : {{ $job['salary'] }}
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 
+    <div>
+        {{-- {{ $jobs->links() }} --}}
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view(
-        'jobs',
-        [
-            'jobs' => Job::all(),
-        ]
-    );
+    $jobs = Job::with('employer')->get();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/contact', function () {
@@ -23,8 +20,5 @@ Route::get('/contact', function () {
 Route::get('jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view(
-        'job',
-        ['job' => $job]
-    );
+    return view('job', ['job' => $job]);
 });
