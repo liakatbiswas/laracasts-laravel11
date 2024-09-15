53e498ca91e748f40edc316f3f6103b2cf84bbf7
Liakat Biswas <liakatbiswas@gmail.com>
2024-09-15 14:05:33 +0600

    Always Validate, Never Trust the User

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,10 +12,7 @@ class Job extends Model
 
     protected $table = 'job_listings';
 
-    protected $fillable = [
-        'title',
-        'salary',
-    ];
+    protected $fillable = ['title', 'salary', 'employer_id'];
 
     public function employer()
     {
